0b44e69b22cc6d43aa58d5d8e6932ecbf34bdb91
adepel00 <adepel00@estudiantes.unileon.es>
2016-05-10 20:56:31 +0200

    Add files via upload

--- floatieee.cpp
+++ floatieee.cpp
@@ -1,7 +1,39 @@
 #include "floatieee.h"
 
-floatIEEE::floatIEEE(std::vector<int> numero){
+floatIEEE::floatIEEE(float numero){
     this->numero = numero;
-    signo = numero.at(0);
-    //exponente =
+    std::string cadenaNumeroPasado;
+    numeroPasado = getFloatingPoint(numero, cadenaNumeroPasado);
+    signo = numeroPasado.at(0);
+    for (int i = 1; i < 8; i++){
+       exponente.push(numeroPasado.at(i));
+    }
+    for(int j = 9; j < 32; j++){
+        mantisa.push(numeroPasado.at(i));
+    }
+}
+
+std::vector<int> floatIEEE::getFloatingPoint( float value, std::string* cadena){
+    int i;
+    std::vector<int> resultado;
+    union
+    {
+         float input;   // assumes sizeof(float) == sizeof(int)
+         int   output;
+    }    data;
+
+    data.input = value;
+
+    std::bitset<sizeof(float) * CHAR_BIT>   bits(data.output);
+
+    std::string mystring = bits.to_string<char,
+                                          std::char_traits<char>,
+                                          std::allocator<char> >();
+    for(i = 0; i < 32; i++){
+        resultado.push_back(mystring.at(i) - 48);
+    }
+
+    *cadena = mystring;
+
+    return resultado;
 }
--- floatieee.h
+++ floatieee.h
@@ -6,13 +6,15 @@
 class floatIEEE
 {
 public:
-    floatIEEE(std::vector<int> numero);
+    floatIEEE(float numero);
+    std::vector<int> getFloatingPoint( float value, std::string* cadena);
 
 private:
     int signo;
+    float numero;
     std::vector<int> exponente;
-    std::vector<int> significativos;
-    std::vector<int> numero;
+    std::vector<int> mantisa;
+    std::vector<int> numeroPasado;
 };
 
 #endif // FLOATIEEE_H
